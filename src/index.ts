2a84f0e3d75b213c3df056e90bd225c508e7e354
枍瞳 <2182025330@qq.com>
2026-08-03 23:08:25 +0800

    学会了使用range以及一些webapi来判断文字是否在指定div内

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-const container = document.getElementById('editor-container') as HTMLDivElement;
-container.contentEditable = 'true';
+const editorContainer  = document.getElementById('editor-container') as HTMLDivElement;
+let timer: number;
+
+const init = () => {
+    editorContainer.contentEditable = 'true';
+    
+    
+    document.addEventListener('selectionchange', () => {
+        clearTimeout(timer);
+        timer = setTimeout(isSelectTextInContainer, 100);
+    });
+}
+
+const getSelectText = (): Selection | null => {
+    const selectText = window.getSelection();
+    return selectText;
+}
+
+const isSelectTextInContainer = () => {
+    const selectText = getSelectText();
+
+    const range = selectText?.getRangeAt(0);
+
+    const container = range?.commonAncestorContainer as HTMLElement;
+
+    return editorContainer.contains(container);
+}
+
+init();
